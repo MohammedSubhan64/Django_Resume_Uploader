cd08389a4c46841d6372ad56e11dcb167fc4d53a
Mohammed Subhan <mohdsubhan2764@gmail.com>
2023-12-09 10:18:32 +0530

    resume form submit and saved in admin page

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ResumeForm
 from .models import Resume
 from django.views import View
@@ -7,4 +7,12 @@ from django.views import View
 class HomeView(View):
     def get(self,request):
         form =ResumeForm()
-        return render(request,'myapp/home.html',{'form':form})
+        candidates = Resume.objects.all()
+        return render(request,'myapp/home.html', { 'candidates':candidates, 'form':form})
+    
+    def post(self,request):
+        form=ResumeForm(request.POST,request.FILES) 
+        if form.is_valid():
+            form.save()
+            return redirect(request,'myapp/home.html',{'form':form})
+            
